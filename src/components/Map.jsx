803bc85af2b2d52956ccf02c38386fa465e00561
Daniel Martinez <DN4251@Gmail.com>
2018-10-02 14:47:19 -0500

    Add import google-maps-react

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { GoogleApiWrapper } from 'google-maps-react';
 
-class GMap extends Component  {
+export class MapContainer extends Component  {
 	render() {
 		return(
-			
+			<div id="map"></div>
 		)
 	}
 }
 
-export default GMap;
+export default GoogleApiWrapper({
+	apiKey: //AIzaSyDqxmYDgJSpgCG4GCvkQCwTGre0Ve49HAE
+})(MapContainer);
