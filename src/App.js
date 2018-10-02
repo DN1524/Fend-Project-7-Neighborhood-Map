1f5b519c974626636a40c61ea4f79c22f7968ea8
Daniel Martinez <DN4251@Gmail.com>
2018-10-02 17:05:58 -0500

    import Map.jsx and display it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import MapContainer from "./components/Map";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <MapContainer />
       </div>
     );
   }
