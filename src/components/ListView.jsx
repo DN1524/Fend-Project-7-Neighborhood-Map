e25bf2306572e3b49cf250032e1d4e4152718873
Daniel Martinez <DN4251@Gmail.com>
2018-10-03 18:19:20 -0500

    Add an UL for future list results

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class ListView extends Component  {
 	render() {
 		return(
-			<div class="list-container">Something...</div>
+			<div class="list-container">
+				<ul class="list-results">
+					<li>Some Text</li>
+					<li>Some Text</li>
+					<li>Some Text</li>
+				</ul>
+			</div>
 		)
 	}
 }
