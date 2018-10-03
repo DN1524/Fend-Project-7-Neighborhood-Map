5a202217a0f380c5d7ab07d7c1f54fa9cabf9c2f
Daniel Martinez <DN4251@Gmail.com>
2018-10-03 15:43:49 -0500

    Change order of components

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
     return (
       <div className="App">
         <MapContainer />
-        <ListView />
         <LocationFilter />
+        <ListView />
       </div>
     );
   }
