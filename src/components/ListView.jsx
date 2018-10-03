ef8b4c2f3379488c1bf93b50ace4cbdfb6632678
Daniel Martinez <DN4251@Gmail.com>
2018-10-03 13:27:09 -0500

    Swap html content

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -3,13 +3,7 @@ import React, { Component } from 'react';
 class ListView extends Component  {
 	render() {
 		return(
-			<div className="list-container">
-				<h1>Placeholder</h1>
-				<input className="text-box" type="text" placeholder="Search">
-      	</input>	
-			</div>
-
-				
+			<div>Something...</div>
 		)
 	}
 }
--- src/components/LocationFilter.jsx
+++ src/components/LocationFilter.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class LocationFilter extends Component  {
 	render() {
 		return(
-			
+			<div className="list-container">
+				<h1>Placeholder</h1>
+				<input className="text-box" type="text" placeholder="Search">
+      	</input>	
+			</div>
 		)
 	}
 }
