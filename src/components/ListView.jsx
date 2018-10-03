45f1afc6e371fbc6d1460bad45cd639a6537759b
Daniel Martinez <DN4251@Gmail.com>
2018-10-03 12:32:03 -0500

    Add placeholder text

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class ListView extends Component  {
 	render() {
 		return(
-			
+			<div className="list-container">
+				<h1>Placeholder</h1>
+			</div>
 		)
 	}
 }
