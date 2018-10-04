0049e321d9b88353222813c553974806d6d53b4d
Daniel Martinez <DN4251@Gmail.com>
2018-10-04 17:23:15 -0500

    Add state for hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import HamburgerMenu from 'react-hamburger-menu';
 
 class NavBar extends Component  {
+	state = {
+    isOpen: false
+  }
+
 	render() {
 		return(
 			<nav className="navbar">
 				<h1>PlaceHolder</h1>
 				<div className="hamburger-container">
 					<HamburgerMenu
+						isOpen={this.state.open}
 						strokeWidth={4}
 						height={22}
 					/>
