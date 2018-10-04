4921c4587f2dcefc9a49d1eefeead4d14b42f5a3
Daniel Martinez <DN4251@Gmail.com>
2018-10-03 19:30:53 -0500

    Change class into className

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -3,15 +3,16 @@ import React, { Component } from 'react';
 class ListView extends Component  {
 	render() {
 		return(
-			<div class="list-container">
+			<div className="list-container">
 				<input className="text-box" type="text" placeholder="Search">
       	</input>
-
-				<ul class="list-results">
-					<li>Some Text</li>
-					<li>Some Text</li>
-					<li>Some Text</li>
-				</ul>
+      	<div>
+					<ul className="list-results">
+						<li>Some Text</li>
+						<li>Some Text</li>
+						<li>Some Text</li>
+					</ul>
+				</div>
 			</div>
 		)
 	}
