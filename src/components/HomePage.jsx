a495fdd2a912abb971fc08e7e85864c9193f15b2
Daniel Martinez <DN4251@Gmail.com>
2018-10-08 11:48:45 -0500

    Add NavBar to HomePage

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SideBar from "./SideBar"
+
+class HomePage extends Component {
+	render() {
+		return(
+			<SideBar />
+		)
+	}
+}
+
+export default HomePage;
