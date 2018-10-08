064a6ed28d07d7efd628fb46a7088a333eaf41b5
Daniel Martinez <DN4251@Gmail.com>
2018-10-08 13:18:13 -0500

    Add Map as a sibling to Sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import MapContainer from "./Map";
 
 class SideBar extends Component  {
 	render() {
 		return(
-			<section className="sidebar">
-				<input className="text-box" type="text" placeholder="Search">
-      	</input>
-      	<div className="results-container">
-					<ul className="list-results">
-						<li>Some Text</li>
-						<li>Some Text</li>
-						<li>Some Text</li>
-					</ul>
-				</div>
-			</section>
+			<div className="sidebar-container">
+				<section className="sidebar">
+					<input className="text-box" type="text" placeholder="Search">
+	      	</input>
+	      	<div className="results-container">
+						<ul className="list-results">
+							<li>Some Text</li>
+							<li>Some Text</li>
+							<li>Some Text</li>
+						</ul>
+					</div>
+				</section>
+
+				<MapContainer />
+			</div>
 		)
 	}
 }
