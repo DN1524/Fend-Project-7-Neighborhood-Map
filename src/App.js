a5b594a2798368985f96824729d666c3408e513f
Daniel Martinez <DN4251@Gmail.com>
2018-10-08 14:10:38 -0500

    Remove Map from App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MapContainer /> {/*Will move later*/}
         <NavBar />
-        
       </div>
     );
   }
