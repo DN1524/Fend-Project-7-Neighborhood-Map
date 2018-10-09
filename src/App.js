b0b8c396e3c067455f8c71ae6a667081e9e6d169
Daniel Martinez <DN4251@Gmail.com>
2018-10-09 16:23:53 -0500

    Start creating Google map without React

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class App extends Component {
   render() {
+
+    initMap = () => {
+      const map = new window.google.maps.Map(document.getElementById('map'), {
+        center: {lat: 41.878114, lng: -87.629798},
+        zoom: 10
+      });
+
     return (
       <div className="App">
+        <div id="map"></div>
+
         <NavBar />
       </div>
     );
