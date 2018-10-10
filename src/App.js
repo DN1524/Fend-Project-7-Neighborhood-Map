5a4c63d260c7b1d0f228f471977b3ebef1b4c6f0
Daniel Martinez <DN4251@Gmail.com>
2018-10-10 12:19:24 -0500

    Import Axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css'
-import NavBar from "./components/NavBar"
+import './App.css';
+import NavBar from "./components/NavBar";
+import axios from "axios";
 
 class App extends Component {
   componentDidMount() {
