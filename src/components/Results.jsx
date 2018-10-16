1e4bc5c62d062631dc76ee23a53f5a1fdf9385ef
Daniel Martinez <DN4251@Gmail.com>
2018-10-16 18:54:13 -0500

    Add componentDidMount for getVenues()

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -6,7 +6,11 @@ class Results extends Component {
 		results: []
 	}
 
-	  getVenues = () => {
+	componentDidMount() {
+    this.getVenues()
+  }
+
+	getVenues = () => {
     const venuesURL = 'https://api.foursquare.com/v2/venues/explore?client_id=3DTFRRBJ2R33GOU1XLL1EIXSYASEF3MSVDAACVHOHLN4U4LV&client_secret=CXVCVX0JTCD1VLNVPP1TQ3L1UKDJVQB1L5ANDRASIRPS2RYH&v=20180323&near=Chicago,IL&query=food';
 
     axios.get(venuesURL)
