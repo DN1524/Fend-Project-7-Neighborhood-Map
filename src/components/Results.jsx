18ba116910b45c5f9f3569ae9ec980cf78332e3c
Daniel Martinez <DN4251@Gmail.com>
2018-10-16 10:52:04 -0500

    Add Results state

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import NavBar from "./NavBar"
 
 class Results extends Component {
+	state = {
+		results: []
+	}
+
 	render() {
 		return(
 			<li>
-				<p className="res-title">Title of Place</p>
+				<p className="res-title">Title of the place!</p>
       	<p className="res-address">Address Line 1</p>
       	<p className="res-address">Address Line 2</p>
       	<p className="res-address">Address Line 3</p>
@@ -15,3 +19,7 @@ class Results extends Component {
 }
 
 export default Results;
+
+// {setTimeout(() => {
+// 	console.log(this.props.results[0].venue.name)
+// },1000)}
