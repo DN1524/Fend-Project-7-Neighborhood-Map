6696c3581e93c729a780f05011ac574f14d6397c
Daniel Martinez <DN4251@Gmail.com>
2018-10-17 15:14:01 -0500

    Remove unnecessary functions

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,27 +2,6 @@ import React, { Component } from 'react';
 import axios from "axios";
 
 class Results extends Component {
-	state = {
-		results: []
-	}
-
-	componentDidMount() {
-    this.getVenues()
-  }
-
-	getVenues = () => {
-    const venuesURL = 'https://api.foursquare.com/v2/venues/explore?client_id=3DTFRRBJ2R33GOU1XLL1EIXSYASEF3MSVDAACVHOHLN4U4LV&client_secret=CXVCVX0JTCD1VLNVPP1TQ3L1UKDJVQB1L5ANDRASIRPS2RYH&v=20180323&near=Chicago,IL&query=food';
-
-    axios.get(venuesURL)
-      .then(res => {
-        this.setState({results: res.data.response.groups[0].items})
-        // console.log(res.data.response.groups[0].items, "Updated State!!")
-      })
-      .catch(err => {
-        console.log("An error has occur!: " + err);
-      })
-  }
-
 	render() {
 		return(
 			<li>
