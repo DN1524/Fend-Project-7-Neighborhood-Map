f21fa973367f5e4c6bd37e35a3d9a9d27330ef17
Daniel Martinez <DN4251@Gmail.com>
2018-10-17 15:11:24 -0500

    Change this.state.results to this.props.results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -26,12 +26,12 @@ class Results extends Component {
 	render() {
 		return(
 			<li>
-				{this.state.results.length > 0 ?
+				{this.props.results.length > 0 ?
 					<React.Fragment>
-						<p className="res-title">{this.state.results[0].venue.name}</p>
-			  		<p className="res-address">{this.state.results[0].venue.location.address}</p>
-			  		<p className="res-address">{this.state.results[0].venue.location.formattedAddress[1]}</p>
-			  		<p className="res-address">{this.state.results[0].venue.location.formattedAddress[2]}</p> 
+						<p className="res-title">{this.props.results[0].venue.name}</p>
+			  		<p className="res-address">{this.props.results[0].venue.location.address}</p>
+			  		<p className="res-address">{this.props.results[0].venue.location.formattedAddress[1]}</p>
+			  		<p className="res-address">{this.props.results[0].venue.location.formattedAddress[2]}</p> 
 			  	</React.Fragment>: ""}
 			</li>
 		)
