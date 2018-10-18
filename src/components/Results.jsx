08662c86bf754e4d5a53fe94b3e128fca394beec
Daniel Martinez <DN4251@Gmail.com>
2018-10-18 15:05:46 -0500

    Add a .map loop

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,12 +5,13 @@ class Results extends Component {
 		return(
 			<li>
 				{this.props.results.length > 0 ?
+					this.props.results.map(res =>
 					<React.Fragment>
-						<p className="res-title">{this.props.results[0].venue.name}</p>
-			  		<p className="res-address">{this.props.results[0].venue.location.address}</p>
-			  		<p className="res-address">{this.props.results[0].venue.location.formattedAddress[1]}</p>
-			  		<p className="res-address">{this.props.results[0].venue.location.formattedAddress[2]}</p> 
-			  	</React.Fragment>: ""}
+						<p className="res-title">{res.venue.name}</p>
+			  		<p className="res-address">{res.venue.location.address}</p>
+			  		<p className="res-address">{res.venue.location.formattedAddress[1]}</p>
+			  		<p className="res-address">{res.venue.location.formattedAddress[2]}</p> 
+			  	</React.Fragment>): "No venue information"}
 			</li>
 		)
 	}
