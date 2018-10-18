54c40d5f6e099feadb0ba3443f581826c3f59ad4
Daniel Martinez <DN4251@Gmail.com>
2018-10-18 15:19:32 -0500

    Add list items directly insted of using components

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,12 +10,18 @@ class SideBar extends Component  {
 	      	</input>
 	      	<div className="results-container">
 						<ul className="list-results">
-							{this.props.results.length > 0 ? 
-								this.props.results.map((res) => 
-									<Results key={res.venue.name} results={this.props.results} />) : 
-									<p className="no-results">No results available</p>
-							}
+						{this.props.results.length > 0 ? 
+							this.props.results.map((res) => 
+								<li>
+									<p className="res-title">{res.venue.name}</p>
+						  		<p className="res-address">{res.venue.location.address}</p>
+						  		<p className="res-address">{res.venue.location.formattedAddress[1]}</p>
+						  		<p className="res-address">{res.venue.location.formattedAddress[2]}</p> 
+				  			</li>
+				  		) : <p className="no-results">No results available</p>
+						}
 						</ul>
+
 					</div>
 				</section>
 			</div>
