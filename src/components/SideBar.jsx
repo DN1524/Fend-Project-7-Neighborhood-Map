f8a677245d236bd2bc636481cc8165753208e86a
Daniel Martinez <DN4251@Gmail.com>
2018-10-24 16:14:02 -0500

    Remove getVenues()

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -12,23 +12,6 @@ class SideBar extends Component  {
 
 	}
 
-	 getVenues = () => {
-    const venuesURL = 'https://api.foursquare.com/v2/venues/explore?client_id=3DTFRRBJ2R33GOU1XLL1EIXSYASEF3MSVDAACVHOHLN4U4LV&client_secret=CXVCVX0JTCD1VLNVPP1TQ3L1UKDJVQB1L5ANDRASIRPS2RYH&v=20180323&near=Chicago,IL&query=food';
-
-    axios.get(venuesURL)
-      .then(res => {
-      	if(this.props.results.length <= 0)  {
-	        this.props.updateVenues(res.data.response.groups[0].items)
-	        console.log("Retrieving Venues...");
-				} else {
-					console.log("There are already venues!");
-				}
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
-
 	render() {
 		return(
 			<div className="sidebar-container">
