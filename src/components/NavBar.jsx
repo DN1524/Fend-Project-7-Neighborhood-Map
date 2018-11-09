011890ef82698c674fe6184d3ad9068d5e8557eb
Daniel Martinez <DN4251@Gmail.com>
2018-11-09 11:25:30 -0600

    Change map width when burger menu is clicked

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,14 +7,15 @@ class NavBar extends Component  {
 	state = {
     open: true
   }
-
   // syncs the boolean of this.state and the burgerMenu.
   handleClick = () => {
   	this.setState({open: this.state.open ? false : true})
   	if (this.state.open === false) {
   		$(".sidebar").removeClass("hidden");
+  		$("#map").css("width", "75.1%");
   	} else {
   		$(".sidebar").addClass("hidden");
+  		$("#map").css("width", "100%");
   	}
   }
 
@@ -22,8 +23,8 @@ class NavBar extends Component  {
 		return(
 			<div className="ui-container">
 				<nav className="navbar">
-					<h1 tabindex="0">Chicago Restaurants</h1>
-					<div className="hamburger-container">
+					<h1 tabIndex="0">Chicago Restaurants</h1>
+					<button className="hamburger-container" tabIndex="0" onClick={this.handleClick}>
 						<HamburgerMenu
 							isOpen={this.state.open}
 							menuClicked={this.handleClick.bind(this)}
@@ -31,7 +32,7 @@ class NavBar extends Component  {
 							height={22}
 							color={"#B7B7B7FF"}
 						/>
-					</div>
+					</button>
 				</nav>
 				<SideBar 
 					filterSearch={this.props.filterSearch}
